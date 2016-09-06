7f299a5da6fec99cbc4b136c5b51c7f1e06741fc
Paul Tangen <ptangen@ptangen.com>
2016-09-06 13:58:22 -0400

    Complete the strings, functions and doc readme

---
+++ helpPaul.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let name = "Audrey"
+
+let uppercaseName = name.uppercaseString
+
+print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
